d30eaf525f9ead8ece02faaeb112a5b3617676e3
Nouf.Aljowaysir <nouf.aljowaysir@NY-C02XT60NJHD2.globalservs.com>
2019-06-11 18:45:42 -0400

    anatomy tab

---
+++ js/skeleton.js
@@ -0,0 +1,8 @@
+document.body.style.backgroundColor = "#7569A9";
+
+var wid = window.innerWidth;
+var hei = window.innerHeight;
+
+var anatomyDiv = document.getElementById("anatomy");
+anatomyDiv.style.width = wid + "px";
+anatomyDiv.style.height = hei + "px";
